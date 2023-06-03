5d62c7208018aa415b207db9f47338dd85f29ce6
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-06-03 15:48:45 -0300

    Enviroments

---
+++ src/Enviroments.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ * Clase para explicación de variables en java
+ * Declaración
+ * Actualización de variables
+ * Convención nombres en Java
+ */
+
+public class Enviroments {
+    
+    /*Datos primitivos
+     * Numéricos
+     * **byte
+     * **short
+     * **int 
+     * **long
+     * Punto Flotante
+     * **float
+     * **double
+     * Boolean
+     * **boolean
+     * Carácter
+     * **char
+     * Referencia
+     * **String
+     * **Arrays(Coleciones)
+     * 
+     */
+
+     
+}
